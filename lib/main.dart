a60f2b532834d733fb432ae254e4a39954005090
clayding <gdskclay@gmail.com>
2021-04-02 13:06:29 +0800

    Added font family to main theme
    Signed-off-by: clayding <gdskclay@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -21,6 +21,7 @@ class MyApp extends StatelessWidget {
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
         primarySwatch: Colors.green,
+        fontFamily: "JetBrainsMono",
       ),
       home: MyHomePage(title: 'Tracker Control Console'),
     );
